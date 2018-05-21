59e7b7bcee7aa1f341b848a059821ddb7faa7362
Charlotte Weaver <charlottesweaver@gmail.com>
2018-05-21 14:21:13 -0700

    Umap (#35)
    * WIP for adding UMAP for layout
    
    * Config via graph method by name instead of em2 or not
    
    * Style cleanup
    
    * List comprehension for vertices
    
    (and spell vertices correctly)

--- application.py
+++ application.py
@@ -35,16 +35,20 @@ class QueryStringError(Exception):
 
 class Vertex:
 
-    def __init__(self, cellId, x, y):
+    def __init__(self, cellId, x, y, z=None):
         self.cellId = cellId
         self.x_coord = x
         self.y_coord = y
+        self.z_coord = z
 
     def x(self):
         return float(self.x_coord)
 
     def y(self):
         return float(self.y_coord)
+
+    def z(self):
+        return float(self.z_coord)
 
 
 application = Flask(__name__, static_url_path='/templates')
@@ -613,10 +617,14 @@ def create_graph(output_cellset, includeisolated=False, graphname="cellgraph"):
     """
     global e
     # Graph using EM2 or use metadata coordinates
-    if application.config["GRAPH_EM2"]:
+    if application.config["GRAPH_METHOD"] == "em2":
         graph, cellidlist = create_graph_em2(output_cellset, includeisolated=False, graphname="cellgraph")
-    else:
+    elif application.config["GRAPH_METHOD"] == "umap":
+        graph, cellidlist = create_graph_umap(output_cellset)
+    elif application.config["GRAPH_METHOD"] == "tsne":
         graph, cellidlist = create_graph_tsne(output_cellset)
+    else:
+        raise ValueError("Invalid graph method :{}".format(application.config["GRAPH_METHOD"]))
     return graph, cellidlist
 
 
@@ -632,7 +640,7 @@ def create_graph_em2(output_cellset, includeisolated=False, graphname="cellgraph
                       keepIsolatedVertices=includeisolated)
     e.computeCellGraphLayout(graphname)
     vertices = e.getCellGraphVertices(graphname)
-    normalized_verticies = normalize_verticies(vertices)
+    normalized_verticies = normalize_vertices(vertices)
     graph = []
     # reset cellids if create the graph
     cellidlist = []
@@ -653,25 +661,45 @@ def create_graph_tsne(output_cellset):
     cellidlist = e.getCellSet(output_cellset)
     vertices = [Vertex(get_cell_name(cid), e.getCellMetaDataValue(cid, "tSNE_1"),
                        e.getCellMetaDataValue(cid, "tSNE_2")) for cid in cellidlist]
-    normalized_verticies = normalize_verticies(vertices)
-    graph = []
-    for i in range(len(normalized_verticies["labels"])):
-        graph.append((normalized_verticies["labels"][i], normalized_verticies["x"][i],
-                      normalized_verticies["y"][i]))
+    normalized_vertices = normalize_vertices(vertices)
+    graph = [
+        (label, x, y)
+        for label, x, y in zip(normalized_vertices['labels'], normalized_vertices['x'], normalized_vertices['y'])
+    ]
     return graph, cellidlist
 
 
-def normalize_verticies(verticies):
+def create_graph_umap(output_cellset):
+    """
+    Layout for given cellset usig precomputed coordinates
+    :param output_cellset: string, name of EM2 cellset
+    :return: list of tuples [(label, x, y), ...], list of kept cellids
+    """
+    # get metadata for each cell
+    cellidlist = e.getCellSet(output_cellset)
+    vertices = [Vertex(get_cell_name(cid),
+                       e.getCellMetaDataValue(cid, "umap_coord1"),
+                       e.getCellMetaDataValue(cid, "umap_coord2"),
+                       e.getCellMetaDataValue(cid, "umap_coord3")) for cid in cellidlist]
+    normalized_vertices = normalize_vertices(vertices)
+    graph = [
+        (label, x, y)
+        for label, x, y in zip(normalized_vertices['labels'], normalized_vertices['x'], normalized_vertices['y'])
+    ]
+    return graph, cellidlist
+
+
+def normalize_vertices(vertices):
     """
     normalize x,y verticies for graph
-    :param verticies: list of vertex objects
+    :param vertices: list of vertex objects
     :return: json: {"labels": list of point labels, "x":
         list of normalized x positions, "y": list of normalized y positions}
     """
     labels = []
     x = []
     y = []
-    for v in verticies:
+    for v in vertices:
         labels.append(v.cellId)
         x.append(v.x())
         y.append(v.y())
